60440d1f823b72c7a8822b46c463b09b89311f6a
thayburiti <thaynara.buriti@gmail.com>
2026-02-09 04:32:49 -0300

    Desafio

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Segundo Desafio DIO")
+let resultado = partidas(150, 20)
+let posicao = ""
+
+function partidas(vitorias, derrotas){
+let rankeadas = vitorias - derrotas
+return(rankeadas)
+}
+if (resultado <= 10)
+    posicao = "Ferro"
+else if ((resultado >=11) && (resultado < 20))
+    posicao = "Bronze"
+else if ((resultado >=21) && (resultado < 50))
+    posicao = "Prata"
+else if ((resultado >=51) && (resultado < 80))
+    posicao = "Ouro"
+else if ((resultado >=81) && (resultado < 90))
+    posicao = "Diamante"
+else if ((resultado >=91) && (resultado < 100))
+    posicao = "Lendário"
+else if (resultado >= 101)
+    posicao = "Imortal"
+console.log(`Seu resultado final é: ${resultado}.
+    Assim, sua posição no ranking é: ${posicao}
+    Parabéns!`)
